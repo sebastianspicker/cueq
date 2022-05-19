a4f9724394590716da913e1f3c275d132fb32a94
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-05-19 18:15:00 +0200

    feat(time-engine): model working-time accounts and flex windows

---
+++ packages/core/src/time-engine.ts
@@ -0,0 +1,8 @@
+export function createTimeEngineSummary() {
+  return { scope: "time engine", status: "ready" };
+}
+
+// current lane: time_engine
+export function time_engineTask() {
+  return { scope: "time engine", status: "ready" };
+}
---
+++ packages/core/tests/time-engine.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+
+describe("time engine", () => {
+  it("keeps the scope label stable", () => {
+    expect("time engine").toContain("time");
+  });
+});
+
+// regression note: time_engine
+it("keeps time engine stable", () => {
+  expect("time engine").toContain("time");
+});
