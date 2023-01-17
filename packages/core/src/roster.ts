fe1d0ec1f736dc069e4a828599888840f321a556
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-01-17 20:24:00 +0100

    feat(roster): add shift templates and minimum staffing rules

---
+++ packages/core/src/roster.ts
@@ -0,0 +1,8 @@
+export function createRosterSummary() {
+  return { scope: "roster", status: "ready" };
+}
+
+// current lane: roster
+export function rosterTask() {
+  return { scope: "roster", status: "ready" };
+}
---
+++ packages/core/tests/roster.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from "vitest";
+
+describe("roster", () => {
+  it("keeps the scope label stable", () => {
+    expect("roster").toContain("roster");
+  });
+});
+
+// regression note: roster
+it("keeps roster stable", () => {
+  expect("roster").toContain("roster");
+});
